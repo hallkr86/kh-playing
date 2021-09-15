f800f514be36a36388fec7b7cd85e905b2f7cd32
Kesha Hall <k_renee10@yahoo.com>
2021-09-14 20:40:26 -0400

    added new files and images

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+//console.log("hello world");
+var buttonEl = document.getElementById("btn-one");
+
+buttonEl.addEventListener("click", function () {
+    console.log("you clicked me!");
+});
