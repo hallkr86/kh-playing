23e7feccc5bb66502ed40801acfa60b6ed675cb6
Kesha Hall <k_renee10@yahoo.com>
2021-09-14 23:00:40 -0400

    added images

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //console.log("hello world");
 var buttonEl = document.getElementById("btn-one");
+var buttonherEl = document.getElementById("btn-her");
+var buttonhimEl = document .getElementById("btn-him");
 
 buttonEl.addEventListener("click", function () {
     console.log("you clicked me!");
+});
+
+buttonherEl.addEventListener("click", function () {
+    console.log("you clicked her!");
+});
+
+buttonhimEl.addEventListener("click", function () {
+    console.log("you clicked him!");
 });
